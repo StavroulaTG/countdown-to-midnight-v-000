c78661f23fa29c4b8f0486b5222ce2d8225bd3f7
Stavroula Guerrero <4stacyg@optonline.net>
2018-05-09 15:57:30 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,9 +4,10 @@ def countdown (number)
   while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
-    countdown_with_sleep
-    sleep (5)
-
   end
   "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep (number)
+sleep (5)
 end
