39efe833d24039cdab4d0b9aef582e5e86caefa1
Stavroula Guerrero <4stacyg@optonline.net>
2018-05-09 15:33:54 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,9 @@
 #write your code here
 
-def countdown
-  "#{number} SECOND(S)!"
+def countdown (seconds)
+  while seconds > 0
+    puts "#{number} SECOND(S)!"
+    seconds -= 1
+  end 
   puts "HAPPY NEW YEAR!"
 end
